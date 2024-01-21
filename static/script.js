8f869d609baea39795b60682fa6c4ee50593f890
wstypr <wsetyapermadi@gmail.com>
2024-01-21 21:09:59 +0700

    Add form validation

--- static/script.js
+++ static/script.js
@@ -7,19 +7,21 @@ form.addEventListener("submit", (e) => {
   let data = new FormData(form);
   e.preventDefault();
 
-  console.log(data.get("nama"));
-  console.log(data.get("opd"));
-
-  fetch(url, { method: "POST", body: data })
-    .then(function (response) {
-      console.log(response);
-      let msg = new bootstrap.Modal(document.getElementById("success-msg"));
-      form.reset();
-      msg.show();
-    })
-    .catch(function (error) {
-      console.log(error);
-      let msg = new bootstrap.Modal(document.getElementById("error-msg"));
-      msg.show();
-    });
+  if (!form.checkValidity()) {
+    form.classList.add("was-validated");
+  } else {
+    fetch(url, { method: "POST", body: data })
+      .then(function (response) {
+        console.log(response);
+        let msg = new bootstrap.Modal(document.getElementById("success-msg"));
+        form.reset();
+        form.classList.remove("was-validated");
+        msg.show();
+      })
+      .catch(function (error) {
+        console.log(error);
+        let msg = new bootstrap.Modal(document.getElementById("error-msg"));
+        msg.show();
+      });
+  }
 });
